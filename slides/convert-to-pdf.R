e49ff956dda4ebafece8764af254b2226fb6e8d1
joeornstein <jornstein@gmail.com>
2020-10-23 15:39:23 -0400

    finish correlation slides

--- slides/convert-to-pdf.R
+++ slides/convert-to-pdf.R
@@ -7,3 +7,4 @@
 pagedown::chrome_print('slides/01-Getting-Started/01-Getting-Started.Rmd')
 pagedown::chrome_print('slides/02-Data-Visualization/02-Data-Visualization.html')
 pagedown::chrome_print('slides/06-Calculus/06-Calculus.html')
+pagedown::chrome_print('slides/09-Correlation/09-Correlation.html')
