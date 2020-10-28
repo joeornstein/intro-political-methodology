6892f58c2e7d69bc05563242449880b11d90a512
joeornstein <38436657+joeornstein@users.noreply.github.com>
2020-10-28 07:41:00 -0400

    add color to matrix multiplication slides

--- slides/convert-to-pdf.R
+++ slides/convert-to-pdf.R
@@ -7,4 +7,5 @@
 pagedown::chrome_print('slides/01-Getting-Started/01-Getting-Started.Rmd')
 pagedown::chrome_print('slides/02-Data-Visualization/02-Data-Visualization.html')
 pagedown::chrome_print('slides/06-Calculus/06-Calculus.html')
+pagedown::chrome_print('slides/07-Probability/08-Hypothesis-Testing.html')
 pagedown::chrome_print('slides/09-Correlation/09-Correlation.html')
