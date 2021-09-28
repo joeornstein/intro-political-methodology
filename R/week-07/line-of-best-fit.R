a5202b5675cbc6330d0805149a204d5c8dd3cdb2
joeornstein <jornstein@gmail.com>
2021-09-28 14:19:27 -0400

    update calculus slides

--- R/week-07/line-of-best-fit.R
+++ R/week-07/line-of-best-fit.R
@@ -1,22 +1,23 @@
 # ---
-# title: Clean up Chapel Hill Expert Survey for slides
+# title: Find the line of best fit
 # author: Joe Ornstein
 # date: 2021-09-27
 # ---
 
 library(tidyverse)
 
-# dataset downloaded from https://www.chesdata.eu/1999-2019chestrend and cleaned up in R/week-07/cleanup-data.R
+# dataset downloaded from https://www.chesdata.eu/1999-2019chestrend 
+# and cleaned up in R/week-07/cleanup-data.R
 ches <- read_csv('data/ches/ches-cleaned.csv')
 
 
 # create a scatter plot of the data
 p <- ggplot(data = ches,
        mapping = aes(x = social, y = economic)) +
-  geom_point() +
+  geom_point(alpha = 0.6) +
   theme_bw() +
   scale_y_continuous(limits = c(-5, 5)) +
-  coord_equal() +
+  scale_x_continuous(limits = c(-5, 5)) +
   labs(title = 'European Political Parties (2019)',
        caption = 'Measures from the Chapel Hill Expert Survey (Bakker et al., 2019)',
        x = 'Position on Social Issues',
@@ -29,6 +30,6 @@ p +
   geom_smooth(method = 'lm', se = FALSE)
 
 # the lm() function estimates the slope of the best fit line
-lm(economic ~ social, data = d)
+lm(economic ~ social, data = ches)
 
 # but where does that line come from???
