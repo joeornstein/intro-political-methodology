d78a9a76d2c51ee1cda433e40f99c3bebf8103a8
Joe <jornstein@gmail.com>
2022-11-09 18:39:36 -0500

    polls

--- R/week-13/polling-distributions.R
+++ R/week-13/polling-distributions.R
@@ -106,3 +106,70 @@ true_margin <- dem_pct - rep_pct
 # our sample statistic is an unbiased estimate of the truth
 # if its expected value equals the truth
 
+# Estimand = thing we're trying to estimate
+true_margin
+
+# Estimator = procedure we use to estimate
+# democratic_vote_margin()
+
+# Estimate = the value we get out of that estimator
+democratic_poll_margin(poll_size = 500)
+
+# Sampling Distribution = The distribution of estimates
+# that we expect from sampling error alone
+
+
+
+## Standard Error ------------------------
+
+# variance is the average squared distance from the mean
+distance <- sampling_distribution - expected_value
+distance_squared <- distance ^ 2
+variance <- mean(distance_squared)
+
+
+var(sampling_distribution) # notice this value is *slightly* different
+# than the value we computed manually
+
+# variance is sum(distance^2) / (n - 1)
+sum(distance^2) / 9999
+
+
+# in practice, we typically take the square root of that variance
+sqrt(var(sampling_distribution))
+
+sd(sampling_distribution) # that's called the standard deviation
+
+
+# THE STANDARD DEVIATION OF A SAMPLING DISTRIBUTION
+# IS CALLED THE STANDARD ERROR
+
+
+# What happens when we fiddle with the poll size?
+sampling_distribution2 <- replicate(10000,
+                                   democratic_poll_margin(poll_size = 2000))
+
+sd(sampling_distribution2)
+
+
+# standard error of a mean = standard deviation of the population / sqrt(n)
+
+
+# THE CENTRAL LIMIT THEOREM
+
+# The sampling distribution of a sample mean (or sample sum) is 
+# approximately normally distributed
+
+# Handy Rules:
+# 68% of a normal distribution falls within 1 standard deviation of the mean
+# 95% of a normal distribution falls within 2 standard deviations of the mean
+# 99.7% falls within 3 standard deviations of the mean
+
+sum(sampling_distribution > expected_value - 2 * sd(sampling_distribution) &
+      sampling_distribution < expected_value + 2 * sd(sampling_distribution))
+
+# This is called a *95% confidence interval*
+# The expected value + or - 2 standard errors
+
+# The margin of error = 2 * standard error
+
